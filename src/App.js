675bd2fb2d3041fb024122db1d8227bdca12c7ac
wilmer142 <wilmercollege@gmail.com>
2019-02-10 09:48:21 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
